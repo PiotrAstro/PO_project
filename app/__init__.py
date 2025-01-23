1e2a7f45d48b3bf04c0e00e71837177ef51a1690
Piotr Zatwarnicki <piotr.l.zatwarnicki@gmail.com>
2025-01-23 11:25:42 +0100

    db can be build now, added pycache to gitignore

--- app/__init__.py
+++ app/__init__.py
@@ -5,9 +5,8 @@ from config import Config
 from app.models import db
 
 # Initialize extensions
-db = SQLAlchemy()
-login_manager = LoginManager()
-login_manager.login_view = 'auth.login'
+# login_manager = LoginManager()
+# login_manager.login_view = 'auth.login'
 
 def create_app(config_class=Config):
     app = Flask(__name__)
@@ -17,13 +16,13 @@ def create_app(config_class=Config):
     db.init_app(app)
     with app.app_context():
         db.create_all()
-    login_manager.init_app(app)
+    # login_manager.init_app(app)
 
-    # Register blueprints
-    from app.auth import bp as auth_bp
-    app.register_blueprint(auth_bp)
+    # # Register blueprints
+    # from app.auth import bp as auth_bp
+    # app.register_blueprint(auth_bp)
 
-    from app.main import bp as main_bp
-    app.register_blueprint(main_bp)
+    # from app.main import bp as main_bp
+    # app.register_blueprint(main_bp)
 
     return app
